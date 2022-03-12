28faf582635744a58613ab18c79770ab5bc0a8f6
KidNova <101372394+K1DN0V4@users.noreply.github.com>
2022-03-12 17:49:59 -0500

    Update main.java
    Made it so that player can choose whether or not they want to play the game. Might change it to a string depending on if it makes it annoying in the checkMove method in TicTacToe.java because I'm going to add a check for out of bounds inputs

--- main.java
+++ main.java
@@ -1,12 +1,20 @@
+import java.util.Scanner;
+
 class Main {
   public static void main(String[] args) {
-    
-    TicTacToe game = new TicTacToe();
 
-    while(game.endGame() == false){
+    Scanner input;
+    int confirm;
+    input = new Scanner(System.in);
+    TicTacToe game = new TicTacToe();
+    System.out.println("Would you like to play? Put in 3 to play and 4 to quit");
+    confirm = input.nextInt();
+    
+    if(confirm == 3){
+      while(game.endGame() == false){
       game.displayBoard();
       game.playerMove();
+      }
     }
-    
   }
 }

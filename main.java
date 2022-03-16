2ab951edc443244ebc86c66b4cc0dc9a2e314542
KidNova <101372394+K1DN0V4@users.noreply.github.com>
2022-03-16 13:10:12 -0400

    Update main.java
    Proper fix for the replay mechanism (thank you very much Mr. Sell). You can see all my failed attempts that worked one day and broke the other. Sometimes the biggest problems require the simplest fixes.

--- main.java
+++ main.java
@@ -11,32 +11,35 @@ class Main {
     TicTacToe game;
     boolean again = false;
     
-    /*System.out.println("Would you like to play? Put in 3 to play and 4 to quit");
+    System.out.println("Would you like to play? Put in 3 to play and 4 to quit");
     confirm = input.nextInt();
-    input.nextLine();*/
+    input.nextLine();
     System.out.println("Would you like to use X or O? Input a single character to choose");
     player = input.nextLine();
     player = player.substring(0,1).toUpperCase();
     player2 = player.charAt(0);
 
     game = new TicTacToe(player2);
-    
+    while( !again){
       while((game.endGame() == false)) {
         game.displayBoard();
         game.playerMove();
       }
-      System.out.println("Would you like to play again? Put in 3 to play and 4 to quit");
-      confirm = input.nextInt();
+      game.clearBoard();
+      //game.replayGame();
+      again = game.replayGame();
+    }
+    
+    /*confirm = input.nextInt();
     input.nextLine();
     if(confirm == 3) {
       again = true;
-      game.clearBoard();
       while(again != false) {
         game.displayBoard();
         game.playerMove();
       }
       System.out.println("Would you like to play again? Put in 3 to play and 4 to quit");
       confirm = input.nextInt();
-    }
+    }*/
   }
 }
